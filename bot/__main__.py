db82a6bdc88178f3bb99e039a6296602c75dadc2
Anatoly <acryptokrestenko@gmail.com>
2022-10-26 16:14:58 +0400

    Add dev/bot/admin

--- bot/__main__.py
+++ bot/__main__.py
@@ -3,20 +3,33 @@ import requests
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
+# from bot import admin
 from config import tg_bot_token, open_toxic_token
 
+import middlewares
+from misc.throttling import rate_limit
+from db import sqlite_db
+
+# GLOBALS
+storage = MemoryStorage()
+counter = 0
 
 bot = Bot(token=tg_bot_token)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
 
+
+async def on_startup():
+    print('Бот включен')
+    middlewares.setup(dp)
+    sqlite_db.sql_start()
 
 @dp.message_handler(commands=['start'])
 async def start(message):
     await message.reply("Привет, пришли сообщение на проверку")
 
-
-
+@rate_limit(limit=10, key=["text"])
 @dp.message_handler(content_types=["text"])
 async def on_message(message: types.Message):
     try:
@@ -33,14 +46,15 @@ async def on_message(message: types.Message):
         toxic = data["body"]["answer"]
 
         if toxic == 'toxic' and data["body"]["probability"] > 0.51:
-            # await message.reply(f"Токсичное сообщение пользователя @{message['from']['username']}: {message.text}\n")
+            await message.reply(f"Токсичное сообщение пользователя @{message['from']['username']}: {message.text}\n")
             await message.delete()
+            bot.__main__.counter += 1
 
     except Exception as e:
         await message.reply(f"Ошибка: {e}")
 
 
+admin.register_handlers_admin(dp)
 
 if __name__ == '__main__':
-    # dp.register_message_handler(on_message)
-    executor.start_polling(dp, skip_updates=False)
+    executor.start_polling(dp, skip_updates=False, on_startup=on_startup)
